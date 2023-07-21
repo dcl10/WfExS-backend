8aaa42abc88ef78e5673c182435136954b2de9c6
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-07-21 16:14:01 +0100

    tests

---
+++ tests/fetchers/test_git.py
@@ -0,0 +1,50 @@
+import pytest
+import logging
+from wfexs_backend.common import RemoteRepo, RepoType
+from wfexs_backend.fetchers.git import guess_git_repo_params
+
+
+@pytest.mark.parametrize(
+    ["url", "expected"],
+    [
+        (
+            "https://github.com/inab/WfExS-backend.git",
+            RemoteRepo(
+                repo_url="https://github.com/inab/WfExS-backend.git",
+                repo_type=RepoType.Git,
+            ),
+        ),
+        (
+            "git+https://github.com/inab/WfExS-backend.git",
+            RemoteRepo(
+                repo_url="https://github.com/inab/WfExS-backend.git",
+                repo_type=RepoType.Git,
+            ),
+        ),
+        (
+            "ssh://git@github.com:inab/WfExS-backend.git",
+            RemoteRepo(
+                repo_url="git@github.com:inab/WfExS-backend.git",
+                repo_type=RepoType.Git,
+            ),
+        ),
+        (
+            "git+ssh://git@github.com:inab/WfExS-backend.git",
+            RemoteRepo(
+                repo_url="git@github.com:inab/WfExS-backend.git",
+                repo_type=RepoType.Git,
+            ),
+        ),
+        (
+            "github.com/inab/WfExS-backend.git",
+            None,
+        ),
+        (
+            "git@github.com:inab/WfExS-backend.git",
+            None,
+        ),
+    ],
+)
+def test_guess_git_repo_params(url, expected):
+    output = guess_git_repo_params(url, logger=logging.Logger("name"))
+    assert output == expected
