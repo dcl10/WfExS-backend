737f97360b2d57e990b01ec62812ef9647f1a01c
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-08-01 12:30:28 +0100

    shitty hack

--- wfexs_backend/fetchers/git.py
+++ wfexs_backend/fetchers/git.py
@@ -166,7 +166,7 @@ class GitFetcher(AbstractRepoFetcher):
         if not os.path.exists(os.path.join(repo_tag_destdir, ".git")):
             # Try cloning the repository without initial checkout
             self.logger.debug(f"repoTag: {repoTag}, type: {type(repoTag)}")
-            if repoTag is not None:
+            if repoTag is not None or repoTag != "None":
                 gitclone_params = [
                     self.git_cmd,
                     "clone",
